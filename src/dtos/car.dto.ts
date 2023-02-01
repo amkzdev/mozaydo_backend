0c26385580cc8565976a70969b1b1de156ae4725
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-02-01 19:57:12 +0330

    Add Car Create Dto

---
+++ src/dtos/car.dto.ts
@@ -0,0 +1,137 @@
+import { IsNumber, IsPositive, IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+
+
+export class createCarDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string
+
+    @IsOptional()
+    @IsNumber()
+    model: number
+
+    @IsOptional()
+    @IsString()
+    company?: string
+
+
+    @IsNumber()
+    @IsOptional()
+    trunkNumber?: string | number
+
+    @IsNumber()
+    @IsOptional()
+    milage?: number
+
+    @IsNumber()
+    @IsOptional()
+    shaftCount?: number
+
+    @IsString()
+    @IsOptional()
+    tip?: string
+
+    @IsString()
+    @IsOptional()
+    color?: string
+
+    @IsString()
+    @IsOptional()
+    gearType?: string
+
+    @IsString()
+    @IsOptional()
+    capacity?: string
+
+    @IsString()
+    @IsOptional()
+    plaque?: string
+
+    @IsString()
+    @IsOptional()
+    system?: string
+
+    @IsString()
+    @IsOptional()
+    carType?: string
+
+    @IsString()
+    @IsOptional()
+    manufacturerCountry?: string
+
+    @IsString()
+    @IsOptional()
+    chassisNumber?: string
+
+    @IsString()
+    @IsOptional()
+    cylinderCount?: string
+
+    @IsString()
+    @IsOptional()
+    motorNumber?: string
+
+    @IsString()
+    @IsOptional()
+    fuelType?: string
+
+    @IsString()
+    @IsOptional()
+    controlType?: string
+
+    @IsString()
+    @IsOptional()
+    automobileItemDesc?: string
+
+    @IsString()
+    @IsOptional()
+    markingCost?: string
+
+    @IsString()
+    @IsOptional()
+    municipalCost?: string
+
+    @IsString()
+    @IsOptional()
+    tabsareCost?: string
+
+    @IsString()
+    @IsOptional()
+    taxInJobs?: string
+
+    @IsString()
+    @IsOptional()
+    admissionCost?: string
+
+    @IsString()
+    @IsOptional()
+    parkingCost?: string
+
+    @IsString()
+    @IsOptional()
+    violationCost?: string
+
+    @IsString()
+    @IsOptional()
+    leasingDebtCost?: string
+
+    @IsString()
+    @IsOptional()
+    maintenanceLoc?: string
+
+    attachments?: File[]
+
+    @IsNumber()
+    @IsOptional()
+    price?: number | null
+
+    @IsNumber()
+    @IsOptional()
+    totalPrice?: string
+
+    @IsNumber()
+    @IsNotEmpty()
+    partyNo?: string
+
+}
