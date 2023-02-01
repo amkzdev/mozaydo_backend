08d5cecee59814ea0e12189d344f8c012c43d090
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-02-01 19:22:07 +0330

    Add Interface

---
+++ src/car/car.interface.ts
@@ -0,0 +1,37 @@
+export interface Car {
+
+    id?: string,
+    title: string,
+    model: number,
+    company?: string,
+    trunkNumber?: string | number,
+    milage?: number,
+    shaftCount?: number,
+    tip?: string,
+    color?: string,
+    gearType?: string,
+    capacity?: string,
+    plaque?: string,
+    system?: string,
+    carType?: string,
+    manufacturerCountry?: string,
+    chassisNumber?: string,
+    cylinderCount?: string
+    motorNumber?: string,
+    fuelType?: string,
+    controlType?: string,
+    automobileItemDesc?: string,
+    markingCost?: string,
+    municipalCost?: string,
+    tabsareCost?: string,
+    taxInJobs?: string,
+    admissionCost?: string,
+    parkingCost?: string,
+    violationCost?: string,
+    leasingDebtCost?: string,
+    maintenanceLoc?: string,
+    attachments?: string[],
+    price?: number,
+    totalPrice?: string
+
+}
