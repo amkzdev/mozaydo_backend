8d4f7c8c3430ca698e3008b0106625ce8a7511de
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-02-01 19:21:50 +0330

    Add Method To Controller

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,8 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarService } from './car.service'
 
 @Controller('car')
 export class CarController {
 
+    constructor(
+        private readonly carService: CarService
+    ) { }
 
-    
+
+    @Get()
+    getAllCars() {
+        return this.carService.getAllCars()
+    }
+
+    @Get(':id')
+    getCarInfo(
+        @Param('id') id: string
+    ) {
+        return this.carService.getCarInfo(id)
+    }
+
+
 }
