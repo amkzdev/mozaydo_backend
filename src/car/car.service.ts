e0321f56aff6234a90ae6cf4735847813e0c0ea5
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-02-01 19:22:00 +0330

    Add Services

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CarService {}
+export class CarService {
+
+    getAllCars(queryParams?: any) {
+        return [
+            { id: 1, name: "پیکان" },
+            { id: 2, name: "سمند" },
+        ]
+    }
+
+    getCarInfo(id: string) {
+        return ({ id, car: 'ماشین' })
+    }
+
+
+
+}
