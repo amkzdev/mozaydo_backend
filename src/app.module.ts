27f71de0e0a489cd4e4bd5dcd8777c586b044d62
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-02-02 14:03:57 +0330

    Add JWT Token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,8 +7,10 @@ import { CarModule } from './car/car.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CarModule,
+  imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/mozaydo'),
+    ConfigModule.forRoot(),
+    CarModule,
     UserModule
   ],
   controllers: [AppController],
--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -4,13 +4,13 @@ import { Model } from 'mongoose';
 import { UserModel } from '../user.model';
 import { SignupParams } from './auth.interface';
 import { hash } from 'bcryptjs'
-
+import * as jwt from 'jsonwebtoken'
 @Injectable()
 export class AuthService {
 
     constructor(@InjectModel('users') private userModel: Model<UserModel>) { }
 
-    async signup({ phone, password, email }: SignupParams) {
+    async signup({ phone, password, email, name }: SignupParams) {
         const userExist = await this.userModel.findOne({ 'phone': phone }).exec()
         if (userExist) {
             throw new ConflictException()
@@ -18,10 +18,19 @@ export class AuthService {
 
         const hashedPassword = await hash(password, 10)
 
-        return await this.userModel.create({
+        const user = await this.userModel.create({
             email,
             password: hashedPassword,
             phone
         })
+        const token = jwt.sign({
+            name,
+            id: user.id,
+        }, process.env.JSON_TOKEN_KEY, {
+            expiresIn: 300
+        })
+
+        return token
+
     }
 }
