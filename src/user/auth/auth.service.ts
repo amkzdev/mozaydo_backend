1be9c73943b146035c97900f3968b9387c728bcb
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-02-02 13:39:16 +0330

    Create Add User

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,18 +1,27 @@
-import { Injectable ,ConflictException  } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserModel } from '../user.model';
 import { SignupParams } from './auth.interface';
+import { hash } from 'bcryptjs'
 
 @Injectable()
 export class AuthService {
 
     constructor(@InjectModel('users') private userModel: Model<UserModel>) { }
 
-    async signup({ phone }: SignupParams) {
+    async signup({ phone, password, email }: SignupParams) {
         const userExist = await this.userModel.findOne({ 'phone': phone }).exec()
-        if(userExist){
+        if (userExist) {
             throw new ConflictException()
         }
+
+        const hashedPassword = await hash(password, 10)
+
+        return await this.userModel.create({
+            email,
+            password: hashedPassword,
+            phone
+        })
     }
 }
